8af572068fecac588d10c6e03fcc663252a76d03
Jinnan Zhang <vannizhang@gmail.com>
2023-05-02 13:25:04 -0700

    setup storybook

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,20 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended",
-        'prettier'
-    ],
-    "overrides": [
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react",
-        "@typescript-eslint",
-        'prettier'
-    ],
-    "rules": {
-        'prettier/prettier': 'error',
-        "@typescript-eslint/interface-name-prefix": "off",
-        "@typescript-eslint/no-namespace": "off"
-    },
-    "ignorePatterns": [
-        "/src/shared/components/Calcite/*", 
-        "**/*.stories.tsx"
-    ]
-}
+  "env": {
+    "browser": true,
+    "es2021": true
+  },
+  "extends": ["eslint:recommended", "plugin:react/recommended", "plugin:@typescript-eslint/recommended", "prettier", "plugin:storybook/recommended"],
+  "overrides": [],
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+    "ecmaVersion": "latest",
+    "sourceType": "module"
+  },
+  "plugins": ["react", "@typescript-eslint", 'prettier'],
+  "rules": {
+    'prettier/prettier': 'error',
+    "@typescript-eslint/interface-name-prefix": "off",
+    "@typescript-eslint/no-namespace": "off"
+  },
+  "ignorePatterns": ["/src/shared/components/Calcite/*", "**/*.stories.tsx"]
+};
