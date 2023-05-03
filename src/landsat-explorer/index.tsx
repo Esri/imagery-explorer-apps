77348f0fce25bcd2187ee7b2a0f958e7a4c6c91d
Jinnan Zhang <vannizhang@gmail.com>
2023-05-03 12:39:14 -0700

    ci: only use babel-plugin-jsx-remove-data-test-id for PROD env

--- src/landsat-explorer/index.tsx
+++ src/landsat-explorer/index.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import Calendar from '../shared/components/Calendar/Calendar';
 
 const Layout = () => {
     return (
         <div className="absolute bottom-0 left-0 bg-white h-32 w-60 z-10 theme-background">
-            Landsat Layout
+            <Calendar
+                year={2020}
+                selectedDate="2020-07-13"
+                availableDates={[]}
+                cloudyDates={[]}
+            />
         </div>
     );
 };
