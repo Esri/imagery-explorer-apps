ffb86da4f4580887b00154a63628e2092cfb85c7
Jinnan Zhang <vannizhang@gmail.com>
2023-05-10 15:56:57 -0700

    doc: add comments to getAcquisitionDates

--- src/landsat-explorer/services/landsat-2/getAcquisitionDates.ts
+++ src/landsat-explorer/services/landsat-2/getAcquisitionDates.ts
@@ -2,6 +2,7 @@ import { FIELD_NAMES } from './config';
 import { LANDSAT_LEVEL_2_SERVICE_URL } from '../../config';
 import {
     IExtent,
+    IFeature,
     IQueryFeaturesResponse,
 } from '@esri/arcgis-rest-feature-service';
 import { format } from 'date-fns';
@@ -29,6 +30,46 @@ type LandsatAcquisitionDate = {
 
 const { ACQUISITION_DATE, CLOUD_COVER, CATEGORY, NAME, BEST } = FIELD_NAMES;
 
+/**
+ * Formats the features from Landsat-level-2 service and returns an array of LandsatAcquisitionDate objects.
+ * @param features - An array of IFeature objects from Landsat-level-2 service.
+ * @returns An array of LandsatAcquisitionDate objects containing the acquisition date, formatted acquisition date, name, cloud cover, and best attributes.
+ */
+const getFormattedAcquisitionDates = (
+    features: IFeature[]
+): LandsatAcquisitionDate[] => {
+    return features.map((feature) => {
+        const { attributes } = feature;
+
+        const acquisitionDate = attributes[ACQUISITION_DATE];
+
+        /**
+         * formatted aquisition date should be like `2023-05-01`
+         */
+        const formattedAcquisitionDate = format(acquisitionDate, 'yyyy-MM-dd');
+
+        return {
+            acquisitionDate,
+            formattedAcquisitionDate,
+            name: attributes[NAME],
+            cloudCover: attributes[CLOUD_COVER],
+            best: attributes[BEST],
+        } as LandsatAcquisitionDate;
+    });
+};
+
+/**
+ * Queries the Landsat-level-2 service to find an array of acquisition dates for available Landsat data that
+ * intersect with the input mapExtent and were acquired during the input year and month.
+ *
+ * @param {number} params.year - The year of the desired acquisition dates.
+ * @param {number} [params.cloudCover=0.1] - The maximum cloud cover percentage of the desired Landsat data.
+ * @param {Object} params.mapExtent - The extent of the map to query.
+ * @param {number} params.month - The month of the desired acquisition dates.
+ *
+ * @returns {Promise} A promise that resolves to an array of LandsatAcquisitionDate objects.
+ *
+ */
 export const getAcquisitionDates = async ({
     year,
     cloudCover = 0.1,
@@ -68,26 +109,5 @@ export const getAcquisitionDates = async ({
         throw data.error;
     }
 
-    const queryResults: IQueryFeaturesResponse = data;
-
-    const features = queryResults?.features || [];
-
-    return features.map((feature) => {
-        const { attributes } = feature;
-
-        const acquisitionDate = attributes[ACQUISITION_DATE];
-
-        /**
-         * formatted aquisition date should be like `2023-05-01`
-         */
-        const formattedAcquisitionDate = format(acquisitionDate, 'yyyy-MM-dd');
-
-        return {
-            acquisitionDate,
-            formattedAcquisitionDate,
-            name: attributes[NAME],
-            cloudCover: attributes[CLOUD_COVER],
-            best: attributes[BEST],
-        } as LandsatAcquisitionDate;
-    });
+    return getFormattedAcquisitionDates(data?.features || []);
 };
