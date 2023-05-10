75ab449c3e5a5c498241c7944490784dfaa5f524
Jinnan Zhang <vannizhang@gmail.com>
2023-05-10 09:56:41 -0700

    feat(landsat): add config.ts

---
+++ src/landsat-explorer/config.ts
@@ -0,0 +1,11 @@
+/**
+ * Landsat 8 and 9 multispectral and multitemporal atmospherically corrected imagery with on-the-fly renderings and indices for visualization and analysis.
+ * @see https://www.arcgis.com/home/item.html?id=bd6b545b95654d91a0b7faf7b5e010f5
+ */
+export const LANDSAT_LEVEL_2_ITEM_ID = `bd6b545b95654d91a0b7faf7b5e010f5`;
+
+/**
+ * A proxy imagery service which has embedded credential that points to the actual Landsat Level-2 imagery service
+ * @see https://landsat.imagery1.arcgis.com/arcgis/rest/services/LandsatC2L2/ImageServer
+ */
+export const LANDSAT_LEVEL_2_SERVICE_URL = `https://utility.arcgis.com/usrsvcs/servers/a73d9143b9d24ebf98278cca4bf2474e/rest/services/LandsatC2L2/ImageServer`;
