05bac5b5a3ed95605bbf3e56f6099b50c3a14ce4
Jinnan Zhang <vannizhang@gmail.com>
2023-05-15 13:23:23 -0700

    feat(landsat): add getTimeExtent

--- src/landsat-explorer/components/Calendar/useYearOptions.tsx
+++ src/landsat-explorer/components/Calendar/useYearOptions.tsx
@@ -1,6 +1,49 @@
-import React from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { DropdownData } from '../../../shared/components/Dropdown/Dropdown';
+import { getTimeExtent } from '../../services/landsat-2/getTimeExtent';
 
 export const useYearOptions = (): DropdownData[] => {
-    return [];
+    const [years, setYears] = useState<number[]>([]);
+
+    const options = useMemo(() => {
+        if (!years || !years.length) {
+            return [];
+        }
+
+        return years.map((year) => {
+            const value = year.toString();
+
+            return {
+                value,
+                selected: false,
+            };
+        });
+    }, [years]);
+
+    useEffect(() => {
+        (async () => {
+            try {
+                const { start, end } = await getTimeExtent();
+
+                const years: number[] = [];
+
+                const startYear = new Date(start).getFullYear();
+
+                const endYear = new Date(end).getFullYear();
+
+                let currYear = startYear;
+
+                while (currYear <= endYear) {
+                    years.push(currYear);
+                    currYear++;
+                }
+
+                setYears(years);
+            } catch (error) {
+                console.error(error);
+            }
+        })();
+    }, []);
+
+    return options;
 };
---
+++ src/landsat-explorer/services/landsat-2/getTimeExtent.ts
@@ -0,0 +1,51 @@
+import { LANDSAT_LEVEL_2_SERVICE_URL } from '../../config';
+
+type TimeExtentData = {
+    start: number;
+    end: number;
+};
+
+let timeExtentData: TimeExtentData = null;
+
+/**
+ * Get Landsat layer's time extent
+ * @returns TimeExtentData
+ *
+ * @example
+ * Usage
+ * ```
+ * getTimeExtent()
+ * ```
+ *
+ * Returns
+ * ```
+ * {
+ *   start: 1363622294000,
+ *   end: 1683500585000
+ * }
+ * ```
+ */
+export const getTimeExtent = async (): Promise<TimeExtentData> => {
+    if (timeExtentData) {
+        return timeExtentData;
+    }
+
+    try {
+        const res = await fetch(`${LANDSAT_LEVEL_2_SERVICE_URL}?f=json`);
+
+        if (!res.ok) {
+            throw new Error('failed to fetch JSON for Landsat-2 service');
+        }
+
+        const data = await res.json();
+
+        const [start, end] = data?.timeInfo?.timeExtent || [];
+
+        return (timeExtentData = {
+            start,
+            end,
+        });
+    } catch (error) {
+        console.error(error);
+    }
+};
