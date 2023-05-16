f59605a31bb98b39a65ca15848003c3ebf02d35b
Jinnan Zhang <vannizhang@gmail.com>
2023-05-16 13:06:20 -0700

    feat(landsat): update Landsat Store add selectedSide4SwipeMode and indexOfSelectedQueryParams4AnimateMode

--- src/shared/store/Landsat/reducer.ts
+++ src/shared/store/Landsat/reducer.ts
@@ -42,14 +42,36 @@ export type QueryParams4LandsatScene = {
  */
 export type AppMode = 'explore' | 'find a scene' | 'swipe' | 'animate';
 
+/**
+ * Swipe Mode allows user to compare Landsat Scene the
+ */
+export type Side4SwipeMode = 'left' | 'right';
+
 export type LandsatState = {
     mode?: AppMode;
+    /**
+     * query params to find a single Landsat scene
+     */
     queryParams4FindASceneMode?: QueryParams4LandsatScene;
+    /**
+     * query params to find Landsat scenes on in swipe mode
+     */
     queryParams4SwipeMode?: {
         left?: QueryParams4LandsatScene;
         right?: QueryParams4LandsatScene;
     };
+    /**
+     * selected side for Swipe Mode, this value helps us to figure out which item in `queryParams4SwipeMode` should be updated
+     */
+    selectedSide4SwipeMode?: Side4SwipeMode;
+    /**
+     * array of query params to find Landsat scenes that will be animated
+     */
     queryParams4AnimateMode?: QueryParams4LandsatScene[];
+    /**
+     * index of selected query params for animate mode, this value helps us to figure out which item in `queryParams4AnimateMode` should be updated
+     */
+    indexOfSelectedQueryParams4AnimateMode?: number;
 };
 
 export const DefaultQueryParams4LandsatScene: QueryParams4LandsatScene = {
@@ -67,10 +89,16 @@ export const initialLandsatState: LandsatState = {
         ...DefaultQueryParams4LandsatScene,
     },
     queryParams4SwipeMode: {
-        left: null,
-        right: null,
+        left: {
+            ...DefaultQueryParams4LandsatScene,
+        },
+        right: {
+            ...DefaultQueryParams4LandsatScene,
+        },
     },
+    selectedSide4SwipeMode: 'left',
     queryParams4AnimateMode: [],
+    indexOfSelectedQueryParams4AnimateMode: 0,
 };
 
 const slice = createSlice({
@@ -86,11 +114,28 @@ const slice = createSlice({
         modeChanged: (state, action: PayloadAction<AppMode>) => {
             state.mode = action.payload;
         },
+        selectedSide4SwipeModeChanged: (
+            state,
+            action: PayloadAction<Side4SwipeMode>
+        ) => {
+            state.selectedSide4SwipeMode = action.payload;
+        },
+        indexOfSelectedQueryParams4AnimateModeChanged: (
+            state,
+            action: PayloadAction<number>
+        ) => {
+            state.indexOfSelectedQueryParams4AnimateMode = action.payload;
+        },
     },
 });
 
 const { reducer } = slice;
 
-export const { queryParams4FindASceneModeChanged, modeChanged } = slice.actions;
+export const {
+    queryParams4FindASceneModeChanged,
+    modeChanged,
+    selectedSide4SwipeModeChanged,
+    indexOfSelectedQueryParams4AnimateModeChanged,
+} = slice.actions;
 
 export default reducer;
