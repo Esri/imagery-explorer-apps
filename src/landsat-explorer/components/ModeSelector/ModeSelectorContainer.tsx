40b4e9aa86fe48eeb05f0fbee128ed52c5ad21a2
Jinnan Zhang <vannizhang@gmail.com>
2023-05-23 16:26:37 -0700

    refactor: move Mode Selector to shared folder

--- src/landsat-explorer/components/ModeSelector/ModeSelectorContainer.tsx
+++ src/landsat-explorer/components/ModeSelector/ModeSelectorContainer.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
-import { Button } from '../../../shared/components/Button';
 import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux';
 import { selectAppMode } from '../../../shared/store/Landsat/selectors';
 import { AppMode, modeChanged } from '../../../shared/store/Landsat/reducer';
 import { selectIsAnimationPlaying } from '../../../shared/store/UI/selectors';
-import classNames from 'classnames';
-
-const modes: AppMode[] = ['explore', 'find a scene', 'swipe', 'animate'];
+import { ModeSelector } from '../../../shared/components/ModeSelector';
 
 export const ModeSelectorContainer = () => {
     const dispatch = useDispatch();
@@ -17,22 +14,12 @@ export const ModeSelectorContainer = () => {
     const isAnimationPlaying = useSelector(selectIsAnimationPlaying);
 
     return (
-        <div
-            className={classNames({
-                'is-disabled': isAnimationPlaying,
-            })}
-        >
-            {modes.map((mode) => (
-                <Button
-                    key={mode}
-                    appearance={mode === selectedMode ? 'solid' : 'transparent'}
-                    onClickHandler={() => {
-                        dispatch(modeChanged(mode));
-                    }}
-                >
-                    <span className="uppercase">{mode}</span>
-                </Button>
-            ))}
-        </div>
+        <ModeSelector
+            selectedMode={selectedMode}
+            disabled={isAnimationPlaying}
+            onChange={(value) => {
+                dispatch(modeChanged(value));
+            }}
+        />
     );
 };
---
+++ src/shared/components/ModeSelector/ModeSelector.tsx
@@ -0,0 +1,56 @@
+import React, { FC } from 'react';
+import { Button } from '../Button';
+import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { selectAppMode } from '../../store/Landsat/selectors';
+import { AppMode, modeChanged } from '../../store/Landsat/reducer';
+import { selectIsAnimationPlaying } from '../../store/UI/selectors';
+import classNames from 'classnames';
+
+const modes: AppMode[] = ['explore', 'find a scene', 'swipe', 'animate'];
+
+type Props = {
+    /**
+     * selected app mode
+     */
+    selectedMode: AppMode;
+    /**
+     * if true, Mode Selector should be disabled
+     */
+    disabled: boolean;
+    /**
+     * fires when app mode is changed
+     * @param value
+     * @returns
+     */
+    onChange: (value: AppMode) => void;
+};
+
+export const ModeSelector: FC<Props> = ({
+    selectedMode,
+    disabled,
+    onChange,
+}: Props) => {
+    return (
+        <div
+            className={classNames('flex flex-col', {
+                'is-disabled': disabled,
+            })}
+        >
+            {modes.map((mode) => (
+                <div key={mode} className="my-1 flex-grow flex items-center">
+                    <Button
+                        appearance={
+                            mode === selectedMode ? 'solid' : 'transparent'
+                        }
+                        onClickHandler={() => {
+                            onChange(mode);
+                        }}
+                    >
+                        <span className="uppercase">{mode}</span>
+                    </Button>
+                </div>
+            ))}
+        </div>
+    );
+};
