9348925f07492ab7c1b00b5900bb617d23954dd6
Jinnan Zhang <vannizhang@gmail.com>
2023-05-26 14:47:20 -0700

    fix: updateObjectIdOfSelectedScene should not be called when animation is playing

--- src/landsat-explorer/components/Calendar/useAvailableScenes.tsx
+++ src/landsat-explorer/components/Calendar/useAvailableScenes.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import {
+    selectAppMode,
     selectAvailableScenes,
     selectQueryParams4SceneInSelectedMode,
 } from '@shared/store/Landsat/selectors';
@@ -48,15 +49,24 @@ const useAvailableScenes = (acquisitionYear: number) => {
     }, [center, acquisitionYear, isAnimationPlaying]);
 
     useEffect(() => {
+        // It is unnecessary to update the object ID of the selected scene while the animation is playing.
+        // This is because the available scenes associated with each animation frame do not get updated during animation playback.
+        // Moreover, when the animation is playing, the map center or acquisition date cannot be changed.
+        // Therefore, the object ID of the Landsat scene for each animation frame remains fixed, eliminating the need for updating it.
+        if (isAnimationPlaying) {
+            return;
+        }
+
         // we should try to find a scene that was acquired from the selected acquisition date
         // whenever the available scenes and acquisition date changes
         const selectedScene = availableScenes.find(
             (d) => d.formattedAcquisitionDate === acquisitionDate
         );
+
         dispatch(
             updateObjectIdOfSelectedScene(selectedScene?.objectId || null)
         );
-    }, [availableScenes, acquisitionDate]);
+    }, [availableScenes, acquisitionDate, isAnimationPlaying]);
 
     return {
         availableScenes,
--- src/landsat-explorer/components/SceneInfo/useDataFromSelectedLandsatScene.tsx
+++ src/landsat-explorer/components/SceneInfo/useDataFromSelectedLandsatScene.tsx
@@ -6,9 +6,17 @@ import {
 } from '@shared/store/Landsat/selectors';
 import { LandsatScene } from '@typing/imagery-service';
 
-const landsatSceneByObjectId: Map<number, LandsatScene> = new Map();
-
 export const useDataFromSelectedLandsatScene = () => {
+    /**
+     * Save/cache Landsat scene data using the object ID as the key.
+     * Why is it necessary to do this? The reason is that the `availableScenesByObjectId` does not get updated during animation playback.
+     * As a result, it may not contain data for the Landsat scene associated with the animation frame. However, we still want to populate
+     * the scene information for each animation frame. Therefore, it is a good idea to retrieve the cached data from this map.
+     */
+    const landsatSceneByObjectId: Map<number, LandsatScene> = useMemo(() => {
+        return new Map();
+    }, []);
+
     const queryParams4SelectedScene = useSelector(
         selectQueryParams4SceneInSelectedMode
     );
