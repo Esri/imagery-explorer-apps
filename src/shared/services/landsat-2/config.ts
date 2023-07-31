4fadac0bf0f3cff9e78af9b52cbd664aa9effc0d
Jinnan Zhang <vannizhang@gmail.com>
2023-07-31 09:29:04 -0700

    chore: update DEV Landsat Service URL

--- src/shared/services/landsat-2/config.ts
+++ src/shared/services/landsat-2/config.ts
@@ -8,7 +8,7 @@ export const LANDSAT_LEVEL_2_ITEM_ID = `bd6b545b95654d91a0b7faf7b5e010f5`;
 
 export const LANDSAT_LEVEL_2_SERVICE_URL_PROD = `https://utility.arcgis.com/usrsvcs/servers/a73d9143b9d24ebf98278cca4bf2474e/rest/services/LandsatC2L2/ImageServer`;
 
-export const LANDSAT_LEVEL_2_SERVICE_URL_DEV = `https://landsatdev.imagery1.arcgis.com/arcgis/rest/services/LandsatC2L2_tm_etm_oli/ImageServer`;
+export const LANDSAT_LEVEL_2_SERVICE_URL_DEV = `https://landsatdev.imagery1.arcgis.com/arcgis/rest/services/LandsatC2L2/ImageServer`;
 
 /**
  * A proxy imagery service which has embedded credential that points to the actual Landsat Level-2 imagery service
