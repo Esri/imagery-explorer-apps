8dbb72c7e0d70611163dabc27eac0b1717851617
Jinnan Zhang <vannizhang@gmail.com>
2023-08-23 16:26:16 -0700

    feat: update Dynamic Info

--- src/shared/components/DynamicModeInfo/DynamicModeInfo.tsx
+++ src/shared/components/DynamicModeInfo/DynamicModeInfo.tsx
@@ -1,6 +1,14 @@
+import { modeChanged } from '@shared/store/Landsat/reducer';
 import React from 'react';
+import { useDispatch } from 'react-redux';
 
 export const DynamicModeInfo = () => {
+    const dispatch = useDispatch();
+
+    const openFindASceneMode = () => {
+        dispatch(modeChanged('find a scene'));
+    };
+
     return (
         <div className="max-w-sm ml-4 xl:ml-10">
             <div className="text-center mb-3">
@@ -13,6 +21,17 @@ export const DynamicModeInfo = () => {
                 fused into a single mosaicked image layer. As you explore, the
                 map continues to dynamically fetch and render the best available
                 scenes.
+            </p>
+
+            <p className="text-sm opacity-80 mt-2">
+                To select a scene for a specific date, try the{' '}
+                <span
+                    className="underline cursor-pointer hover:opacity-100"
+                    onClick={openFindASceneMode}
+                >
+                    FIND A SCENE
+                </span>{' '}
+                mode.
             </p>
         </div>
     );
