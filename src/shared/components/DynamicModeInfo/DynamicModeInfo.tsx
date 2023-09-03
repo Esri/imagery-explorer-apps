9f8b1bef13208bb4c956fb45bd890088db8b161a
Jinnan Zhang <vannizhang@gmail.com>
2023-09-03 14:46:51 -0700

    fix: Dynamic Mode Info 4 Landsat should not include 'Find a Scene' link in mobile view

--- src/shared/components/DynamicModeInfo/DynamicModeInfo.tsx
+++ src/shared/components/DynamicModeInfo/DynamicModeInfo.tsx
@@ -1,38 +1,23 @@
-import { modeChanged } from '@shared/store/Landsat/reducer';
-import React from 'react';
-import { useDispatch } from 'react-redux';
+import React, { Suspense, lazy } from 'react';
+
+const LandsatInfo = lazy(
+    () =>
+        import(
+            /* webpackChunkName: "landsat-dynamic-mode" */
+            './LandsatInfo'
+        )
+);
 
 export const DynamicModeInfo = () => {
-    const dispatch = useDispatch();
-
-    const openFindASceneMode = () => {
-        dispatch(modeChanged('find a scene'));
-    };
-
     return (
         <div className="max-w-sm ml-4 xl:ml-10">
             <div className="text-center mb-3">
                 <span className="uppercase text-sm">Dynamic View</span>
             </div>
 
-            <p className="text-sm opacity-80">
-                In the current map display, the most recent and most cloud free
-                scenes from the Landsat archive are prioritized and dynamically
-                fused into a single mosaicked image layer. As you explore, the
-                map continues to dynamically fetch and render the best available
-                scenes.
-            </p>
-
-            <p className="text-sm opacity-80 mt-2">
-                To select a scene for a specific date, try the{' '}
-                <span
-                    className="underline cursor-pointer hover:opacity-100"
-                    onClick={openFindASceneMode}
-                >
-                    FIND A SCENE
-                </span>{' '}
-                mode.
-            </p>
+            <Suspense fallback={<calcite-loader></calcite-loader>}>
+                {IMAGERY_SERVICE === 'landsat' && <LandsatInfo />}
+            </Suspense>
         </div>
     );
 };
---
+++ src/shared/components/DynamicModeInfo/LandsatInfo.tsx
@@ -0,0 +1,38 @@
+import { IS_MOBILE_DEVICE } from '@shared/constants/UI';
+import { modeChanged } from '@shared/store/Landsat/reducer';
+import React from 'react';
+import { useDispatch } from 'react-redux';
+
+const LandsatInfo = () => {
+    const dispatch = useDispatch();
+
+    const openFindASceneMode = () => {
+        dispatch(modeChanged('find a scene'));
+    };
+    return (
+        <>
+            <p className="text-sm opacity-80">
+                In the current map display, the most recent and most cloud free
+                scenes from the Landsat archive are prioritized and dynamically
+                fused into a single mosaicked image layer. As you explore, the
+                map continues to dynamically fetch and render the best available
+                scenes.
+            </p>
+
+            {IS_MOBILE_DEVICE === false ? (
+                <p className="text-sm opacity-80 mt-2">
+                    To select a scene for a specific date, try the{' '}
+                    <span
+                        className="underline cursor-pointer hover:opacity-100"
+                        onClick={openFindASceneMode}
+                    >
+                        FIND A SCENE
+                    </span>{' '}
+                    mode.
+                </p>
+            ) : null}
+        </>
+    );
+};
+
+export default LandsatInfo;
