119fd326eff4a0604b39ae39647c793c04160fde
Jinnan Zhang <vannizhang@gmail.com>
2023-10-13 10:25:58 -0700

    refactor(issue #19): move createVideoViaMediaRecorder to helpers.ts

--- src/shared/components/AnimationLayer/DownloadAnimationControl.tsx
+++ src/shared/components/AnimationLayer/DownloadAnimationControl.tsx
@@ -1,6 +1,7 @@
 import React, { FC, useEffect, useState } from 'react';
 import IImageElement from 'esri/layers/support/ImageElement';
 import { downloadBlob } from '@shared/utils/snippets/downloadBlob';
+import { createVideoViaMediaRecorder } from './helpers';
 
 type Props = {
     /**
@@ -21,99 +22,6 @@ type Props = {
     height: number;
 };
 
-const loadImage = async (imageURL: string): Promise<HTMLImageElement> => {
-    const image = new Image();
-    image.src = imageURL;
-
-    return new Promise((resolve) => {
-        image.onload = () => {
-            resolve(image);
-        };
-    });
-};
-
-const createVideoViaMediaRecorder = async ({
-    mediaLayerElements,
-    animationSpeed,
-    width,
-    height,
-}: Props) => {
-    const canvas = document.createElement('canvas');
-    const context = canvas.getContext('2d');
-
-    canvas.width = width;
-    canvas.height = height;
-
-    const canvasStream = canvas.captureStream(30); // frames per second
-    // console.log('Started stream capture from canvas element: ', stream);
-
-    const chunks: Blob[] = [];
-
-    const mediaRecorder = new MediaRecorder(canvasStream, {
-        mimeType: 'video/webm',
-        /**
-         * use 40000000 for 4K video
-         * @see https://stackoverflow.com/questions/65800159/how-do-you-determine-bitspersecond-for-media-recording
-         */
-        bitsPerSecond: 40000000,
-    });
-
-    // Record data in chunks array when data is available
-    mediaRecorder.addEventListener('dataavailable', (evt: BlobEvent) => {
-        chunks.push(evt.data);
-    });
-
-    // save blob on media recorder stops recording
-    mediaRecorder.addEventListener('stop', () => {
-        downloadBlob(new Blob(chunks, { type: 'video/webm' }), 'output.webm');
-    });
-
-    // load media layer elements as an array of HTML Image Elements
-    const images = await Promise.all(
-        mediaLayerElements.map((elem) => loadImage(elem.image as string))
-    );
-    // console.log(images)
-
-    let indexOfCurrFrame = 0;
-
-    let timeLastFrameWasRendered: number = null;
-
-    const animate = () => {
-        if (
-            timeLastFrameWasRendered !== null &&
-            performance.now() - timeLastFrameWasRendered < animationSpeed
-        ) {
-            requestAnimationFrame(animate);
-            return;
-        }
-
-        // no more frames to add, stop recording
-        if (indexOfCurrFrame === images.length) {
-            mediaRecorder.stop();
-            return;
-        }
-
-        timeLastFrameWasRendered = performance.now();
-
-        context.clearRect(0, 0, canvas.width, canvas.height);
-
-        context.drawImage(
-            images[indexOfCurrFrame],
-            0,
-            0,
-            canvas.width,
-            canvas.height
-        );
-
-        indexOfCurrFrame = indexOfCurrFrame + 1;
-
-        requestAnimationFrame(animate);
-    };
-
-    requestAnimationFrame(animate);
-    mediaRecorder.start();
-};
-
 export const DownloadAnimationControl: FC<Props> = ({
     mediaLayerElements,
     animationSpeed,
@@ -123,17 +31,20 @@ export const DownloadAnimationControl: FC<Props> = ({
     const [isDownloading, setIsDownloading] = useState<boolean>(false);
 
     useEffect(() => {
-        if (!isDownloading) {
-            return;
-        }
+        (async () => {
+            if (!isDownloading) {
+                return;
+            }
 
-        createVideoViaMediaRecorder({
-            mediaLayerElements,
-            animationSpeed,
-            width,
-            height,
-        });
+            const outputVideo = await createVideoViaMediaRecorder({
+                mediaLayerElements,
+                animationSpeed,
+                width,
+                height,
+            });
 
+            downloadBlob(outputVideo, 'output.webm');
+        })();
         // start making video file and downloading it
     }, [isDownloading]);
 
