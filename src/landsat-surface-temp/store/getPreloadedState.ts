632ff1e8582df69c74e5c6e59d556e71e774e98b
Jinnan Zhang <vannizhang@gmail.com>
2023-12-14 10:27:39 -0800

    refactor(landsat-surface-temp): getPreloadedState should use data from URLHashPrams

--- src/landsat-surface-temp/store/getPreloadedState.ts
+++ src/landsat-surface-temp/store/getPreloadedState.ts
@@ -1,13 +1,20 @@
 import { initialMapState, MapState } from '@shared/store/Map/reducer';
 import { MAP_CENTER, MAP_ZOOM } from '@shared/constants/map';
 
-import { getMapCenterFromHashParams } from '@shared/utils/url-hash-params';
+import {
+    getHashParamValueByKey,
+    getMapCenterFromHashParams,
+    getMaskToolDataFromHashParams,
+    getQueryParams4MainSceneFromHashParams,
+    getTemporalProfileToolDataFromHashParams,
+} from '@shared/utils/url-hash-params';
 
 // import { initialUIState, UIState } from './UI/reducer';
 import {
     initialImagerySceneState,
     ImageryScenesState,
     DefaultQueryParams4ImageryScene,
+    AppMode,
     // QueryParams4ImageryScene,
 } from '@shared/store/ImageryScene/reducer';
 import { PartialRootState } from '@shared/store/configureStore';
@@ -32,30 +39,44 @@ const getPreloadedMapState = (): MapState => {
 };
 
 const getPreloadedImageryScenesState = (): ImageryScenesState => {
+    const mode: AppMode =
+        (getHashParamValueByKey('mode') as AppMode) || 'dynamic';
+
+    const queryParams4MainScene = getQueryParams4MainSceneFromHashParams() || {
+        ...DefaultQueryParams4ImageryScene,
+    };
+
     return {
         ...initialImagerySceneState,
+        mode,
         queryParams4MainScene: {
-            ...DefaultQueryParams4ImageryScene,
+            ...queryParams4MainScene,
             rasterFunctionName: 'Surface Temperature Colorized (Fahrenheit)',
         },
         queryParams4SecondaryScene: {
-            ...DefaultQueryParams4ImageryScene,
+            ...queryParams4MainScene,
             rasterFunctionName: 'Natural Color with DRA',
         },
     };
 };
 
 const getPreloadedMaskToolState = (): MaskToolState => {
+    const maskToolData = getMaskToolDataFromHashParams();
+
     return {
         ...initialMaskToolState,
+        ...maskToolData,
         spectralIndex: 'temperature farhenheit',
         shouldClipMaskLayer: true,
     } as MaskToolState;
 };
 
 const getPreloadedTrendToolState = (): TrendToolState => {
+    const trendToolData = getTemporalProfileToolDataFromHashParams();
+
     return {
         ...initialTrendToolState,
+        ...trendToolData,
         spectralIndex: 'temperature farhenheit',
     } as TrendToolState;
 };
