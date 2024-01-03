476cd873d7fd8bfc02d5437ec100cdcb43b2bb6e
Jinnan Zhang <vannizhang@gmail.com>
2024-01-03 12:00:51 -0800

    revert: getCurrentYear no longer need to return hardcoded end year for Landsat service

--- src/shared/utils/date-time/getCurrentDateTime.ts
+++ src/shared/utils/date-time/getCurrentDateTime.ts
@@ -5,17 +5,6 @@ import { APP_NAME } from '@shared/config';
  * @returns current year in format of `yyyy`, e.g., 2023
  */
 export const getCurrentYear = () => {
-    // USGS has temporarily paused the processing of Landsat Level-1 Product.
-    // Therefore we should use 2023 as current year for Landsat Explorer app
-    // until USGS has this issue reseoved.
-    if (
-        APP_NAME === 'landsat' ||
-        APP_NAME === 'landsat-surface-temp' ||
-        APP_NAME === 'spectral-sampling-tool'
-    ) {
-        return 2023;
-    }
-
     return new Date().getFullYear();
 };
 
