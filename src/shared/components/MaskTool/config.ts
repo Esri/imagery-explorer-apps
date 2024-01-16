8b8ccfdf2813f30627ae122c0e224734f0de1480
Jinnan Zhang <vannizhang@gmail.com>
2024-01-16 14:38:00 -0800

    fix: Tooltip text for Index Tool

--- src/shared/components/MaskTool/config.ts
+++ src/shared/components/MaskTool/config.ts
@@ -1,3 +1,3 @@
 export const MASK_TOOL_HEADER_TOOLTIP = `
-The spectral bands can be used to compute a variety of indices. Different thresholds can be applied to these spectral indices to mask and highlight different characteristics of the Earth's surface..
+The Spectral reflectance values can be used to compute a variety of indices. Different thresholds can be applied to these spectral indices to mask and highlight different characteristics of the Earth's surface.
 `;
