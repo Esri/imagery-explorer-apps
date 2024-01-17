898a1fbeda75ddbdd6a05292ae17b3d1e9d85bfa
Jinnan Zhang <vannizhang@gmail.com>
2024-01-17 09:35:41 -0800

    fix: Update the INDEX action language ref #74

--- src/landsat-explorer/components/MaskTool/MaskToolContainer.tsx
+++ src/landsat-explorer/components/MaskTool/MaskToolContainer.tsx
@@ -110,7 +110,10 @@ export const MaskToolContainer = () => {
                         'w-full mt-10 flex justify-center text-center text-sm is-disabled'
                     )}
                 >
-                    <p>Select a scene to visualize the Index</p>
+                    <p>
+                        Select a scene to calculate a mask for the selected
+                        index.
+                    </p>
                 </div>
             ) : (
                 <>
