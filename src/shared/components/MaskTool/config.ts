d0539c6a710d237d708d2254f05e0f251e8b18cf
Jinnan Zhang <vannizhang@gmail.com>
2024-02-09 09:15:06 -0800

    chore: add copyright statement to source code files

--- src/shared/components/MaskTool/config.ts
+++ src/shared/components/MaskTool/config.ts
@@ -1,3 +1,18 @@
+/* Copyright 2024 Esri
+ *
+ * Licensed under the Apache License Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 export const MASK_TOOL_HEADER_TOOLTIP = `
 Spectral reflectance values can be used to compute a variety of indices. Different thresholds can be applied to these spectral indices to mask and highlight different characteristics of the Earth's surface.
 `;
