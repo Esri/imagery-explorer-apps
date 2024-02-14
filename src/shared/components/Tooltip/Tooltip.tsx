afb66d7907998841bc86e74af781529441705bad
Jinnan Zhang <vannizhang@gmail.com>
2024-02-14 13:26:17 -0800

    fix(Tooltip): should be placed at fixed position instead of position relative to the container

--- src/shared/components/Tooltip/Tooltip.tsx
+++ src/shared/components/Tooltip/Tooltip.tsx
@@ -15,7 +15,7 @@
 
 import { debounce } from '@shared/utils/snippets/debounce';
 import classNames from 'classnames';
-import React, { FC, useState } from 'react';
+import React, { FC, useRef, useState } from 'react';
 
 type Props = {
     content: string;
@@ -23,16 +23,49 @@ type Props = {
     children?: React.ReactNode;
 };
 
+type TooltipPosition = {
+    left: number;
+    top: number;
+};
+
 export const Tooltip: FC<Props> = ({ content, width = 130, children }) => {
+    const containerRef = useRef<HTMLDivElement>();
+
     const [visible, setIsVisible] = useState<boolean>(false);
 
+    /**
+     * Fixed position of the Tooltip relative to the screen
+     */
+    const [position, setPosition] = useState<TooltipPosition>();
+
     const toggleTooltipVisibility = debounce((val: boolean) => {
+        if (val) {
+            getTooltipPosition();
+        } else {
+            setPosition(null);
+        }
+
         setIsVisible(val);
     }, 150);
+
+    /**
+     * calculate the fixed position (relative to the window) where the tooltip should be placed
+     */
+    const getTooltipPosition = () => {
+        // Get the position of the element relative to the viewport
+        const rect = containerRef.current.getBoundingClientRect();
+
+        // The position of the element relative to the screen
+        setPosition({
+            top: rect.top,
+            left: rect.left,
+        });
+    };
 
     return (
         <div
             className="relative cursor-pointer"
+            ref={containerRef}
             onMouseEnter={() => {
                 toggleTooltipVisibility(true);
             }}
@@ -41,18 +74,22 @@ export const Tooltip: FC<Props> = ({ content, width = 130, children }) => {
             }}
         >
             {children}
-            <div
-                className={classNames(
-                    'absolute inline-block left-1/2 bottom-[125%] translate-x-[-50%] bg-custom-background text-custom-light-blue-90 p-1 px-2 border border-custom-light-blue-50 text-xs z-10 pointer-events-none',
-                    {
-                        hidden: visible === false,
-                    }
-                )}
-                dangerouslySetInnerHTML={{ __html: content }}
-                style={{
-                    width: `${width}px`,
-                }}
-            ></div>
+
+            {visible && position ? (
+                <div
+                    className={classNames(
+                        'fixed inline-block translate-x-[-50%] translate-y-[-105%] bg-custom-background text-custom-light-blue-90 p-1 px-2 border border-custom-light-blue-50 text-xs z-10 pointer-events-none'
+                    )}
+                    dangerouslySetInnerHTML={{ __html: content }}
+                    style={{
+                        width: `${width}px`,
+                        left: position.left,
+                        top: position.top,
+                    }}
+                ></div>
+            ) : (
+                <></>
+            )}
         </div>
     );
 };
