0bf9082857599e7f4ca9c0350c6207d5dddd2a18
Jinnan Zhang <vannizhang@gmail.com>
2024-03-04 12:23:59 -0800

    chore(shared): save Copy Link Button

--- src/shared/components/AnimationDownloadPanel/CopyLinkButton.tsx
+++ src/shared/components/AnimationDownloadPanel/CopyLinkButton.tsx
@@ -12,7 +12,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 import { copyAnimationLink } from '@shared/store/UI/thunks';
 import classNames from 'classnames';
 import React from 'react';
