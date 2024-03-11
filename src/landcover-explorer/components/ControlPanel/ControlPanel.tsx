07a6ed716454f593ee2fbc42870598bb09b5633a
Jinnan Zhang <vannizhang@gmail.com>
2024-03-11 10:25:33 -0700

    refactor: use BootomPanel from @shaed/components

--- src/landcover-explorer/components/ControlPanel/ControlPanel.tsx
+++ src/landcover-explorer/components/ControlPanel/ControlPanel.tsx
@@ -22,57 +22,81 @@ import ClassificationsList from './ClassificationsList/ClassificationsListContai
 import LayerSelector from './LayerSelector/LayerSelectorContainer';
 import TimeSlider from './TimeSlider/TimeSliderContainer';
 import { selectShouldShowSentinel2Layer } from '@shared/store/LandcoverExplorer/selectors';
-import Tooltip from './Tooltip/TooltipContainer';
-import ToggleButton from './ToggleButton/ToggleButtonContainer';
+// import Tooltip from './Tooltip/TooltipContainer';
+// import ToggleButton from './ToggleButton/ToggleButtonContainer';
 // import { selectShouldHideControlPanel } from '@shared/store/LandcoverUI/selectors';
 import Sentinel2LayerRasterFunctionsList from './Sentinel2LayerRasterFunctionsList/Sentinel2LayerRasterFunctionsListContainer';
 import LandCoverGraph from './LandCoverGraph/LandCoverGraphContainer';
-import { selectHideBottomPanel } from '@shared/store/UI/selectors';
+// import { selectHideBottomPanel } from '@shared/store/UI/selectors';
+import BottomPanel from '@shared/components/BottomPanel/BottomPanel';
 
 const ControlPanel = () => {
     // const dispatch = useDispatch();
 
-    const hideControlPanel = useSelector(selectHideBottomPanel);
+    // const hideControlPanel = useSelector(selectHideBottomPanel);
 
     const shouldShowSentinel2Layer = useSelector(
         selectShouldShowSentinel2Layer
     );
 
     return (
-        <>
-            {hideControlPanel === false && (
-                <div className="control-panel absolute bottom-0 left-0 w-full h-bottom-panel-height z-10">
-                    <div className="theme-background absolute top-0 left-0 w-full h-full"></div>
-                    <div className="control-panel-top-shadow absolute top-0 left-0 w-full"></div>
-
-                    <div className="relative w-full h-full p-2 pt-6 xl:flex text-custom-light-blue justify-around z-10 overflow-y-auto xl:overflow-y-unset">
-                        <div className="flex">
-                            <LayerSelector />
-                            <TimeSlider />
-                        </div>
-
-                        <div className="flex flex-col-reverse md:flex-row pb-6 md:pb-0">
-                            {shouldShowSentinel2Layer === false && (
-                                <ClassificationsList />
-                            )}
-
-                            {shouldShowSentinel2Layer && (
-                                <Sentinel2LayerRasterFunctionsList />
-                            )}
-
-                            <LandCoverGraph />
-                        </div>
-                    </div>
-
-                    <Tooltip />
-
-                    {/* <ActionBar /> */}
+        <BottomPanel>
+            <div className="relative w-full h-full p-2 xl:flex text-custom-light-blue justify-around">
+                <div className="flex">
+                    <LayerSelector />
+                    <TimeSlider />
                 </div>
-            )}
 
-            <ToggleButton />
-        </>
+                <div className="flex flex-col-reverse md:flex-row pb-6 md:pb-0">
+                    {shouldShowSentinel2Layer === false && (
+                        <ClassificationsList />
+                    )}
+
+                    {shouldShowSentinel2Layer && (
+                        <Sentinel2LayerRasterFunctionsList />
+                    )}
+
+                    <LandCoverGraph />
+                </div>
+            </div>
+        </BottomPanel>
     );
+
+    // return (
+    //     <>
+    //         {hideControlPanel === false && (
+    //             <div className="control-panel absolute bottom-0 left-0 w-full h-bottom-panel-height z-10">
+    //                 <div className="theme-background absolute top-0 left-0 w-full h-full"></div>
+    //                 <div className="control-panel-top-shadow absolute top-0 left-0 w-full"></div>
+
+    //                 <div className="relative w-full h-full p-2 pt-6 xl:flex text-custom-light-blue justify-around z-10 overflow-y-auto xl:overflow-y-unset">
+    //                     <div className="flex">
+    //                         <LayerSelector />
+    //                         <TimeSlider />
+    //                     </div>
+
+    //                     <div className="flex flex-col-reverse md:flex-row pb-6 md:pb-0">
+    //                         {shouldShowSentinel2Layer === false && (
+    //                             <ClassificationsList />
+    //                         )}
+
+    //                         {shouldShowSentinel2Layer && (
+    //                             <Sentinel2LayerRasterFunctionsList />
+    //                         )}
+
+    //                         <LandCoverGraph />
+    //                     </div>
+    //                 </div>
+
+    //                 <Tooltip />
+
+    //                 {/* <ActionBar /> */}
+    //             </div>
+    //         )}
+
+    //         <ToggleButton />
+    //     </>
+    // );
 };
 
 export default ControlPanel;
