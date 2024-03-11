d671511184be6732161d3faa6bce8c8263888308
Jinnan Zhang <vannizhang@gmail.com>
2024-03-11 10:06:22 -0700

    feat(landciver-explorer): add Map Action Buttons

--- src/landcover-explorer/components/MapView/MapViewContainer.tsx
+++ src/landcover-explorer/components/MapView/MapViewContainer.tsx
@@ -57,6 +57,10 @@ import {
 import MapView from '@shared/components/MapView/MapView';
 import { SwipeWidget4Landcover, SwipeWidget4Sentinel2 } from '../SwipeWidget';
 import SearchWidget from '@shared/components/MapView/SearchWidget';
+import { MapActionButtonsGroup } from '@shared/components/MapActionButton';
+import { Zoom2NativeScale } from '@shared/components/Zoom2NativeScale/Zoom2NativeScale';
+import { ScreenshotWidget } from '@shared/components/ScreenshotWidget/ScreenshotWidget';
+import { CopyLinkWidget } from '@shared/components/CopyLinkWidget';
 
 const MapViewContainer = () => {
     const dispatch = useDispatch();
@@ -163,6 +167,16 @@ const MapViewContainer = () => {
                 <CustomMapArrtribution atrribution="Sentinel-2 10m Land Use/Land Cover data by Impact Observatory, Microsoft, and Esri" />
 
                 <AnimationPanel />
+
+                <MapActionButtonsGroup>
+                    <Zoom2NativeScale
+                        nativeScale={37795}
+                        tooltip={"Zoom to Land cover layer's native resolution"}
+                    />
+                    {/* <ZoomToExtent /> */}
+                    <ScreenshotWidget />
+                    <CopyLinkWidget />
+                </MapActionButtonsGroup>
             </MapView>
 
             <ReferenceLayersToggleControl />
