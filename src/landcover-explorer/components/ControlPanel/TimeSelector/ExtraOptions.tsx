037cb2ba445253bddd1f8b6d38c6cb2c91ffdc4d
Jinnan Zhang <vannizhang@gmail.com>
2024-03-27 15:07:01 -0700

    revert(landcoverexplore): revert commit #3a8c4ad Save Options in Time Selector should be disabled but hidden instead

--- src/landcover-explorer/components/ControlPanel/TimeSelector/ExtraOptions.tsx
+++ src/landcover-explorer/components/ControlPanel/TimeSelector/ExtraOptions.tsx
@@ -84,18 +84,18 @@ export const SaveOptions: FC<SaveOptionsProps> = ({
         return null;
     }
 
-    // // no need to show save options if the Sentinel-2 Imagery Layer is on
-    // // but we still want to add to a place holder component to prevent the TimeSlider from jumping up and down.
-    // if (shouldShowSentinel2Layer) {
-    //     return null;
-    // }
+    // no need to show save options if the Sentinel-2 Imagery Layer is on
+    // but we still want to add to a place holder component to prevent the TimeSlider from jumping up and down.
+    if (shouldShowSentinel2Layer) {
+        return (
+            <div className="h-[16px] w-full">
+                {/* <span className='text-xs'>Save options are only enabled for Land Cover layer</span> */}
+            </div>
+        );
+    }
 
     return (
-        <div
-            className={classNames('flex items-center justify-around', {
-                'is-disabled': shouldShowSentinel2Layer,
-            })}
-        >
+        <div className={classNames('flex items-center justify-around')}>
             {/* <div className='cursor-pointer flex items-center'
                 onClick={donwloadButtonOnClick}
             >
