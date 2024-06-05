659341e9eef95f6bed767a631fd90d2a91aa99a4
Jinnan Zhang <vannizhang@gmail.com>
2024-06-05 16:40:33 -0700

    chore: update GLOBAL_WATER_LAND_MASK_LAYER_ITEM_ID

--- src/sentinel-1-explorer/contans/index.ts
+++ src/sentinel-1-explorer/contans/index.ts
@@ -1,2 +1,6 @@
+/**
+ * JRC yearly classification data, with the oceans filled
+ * @see https://www.arcgis.com/home/item.html?id=f61d078c63bf49c497a1fb0bb9ad7d71
+ */
 export const GLOBAL_WATER_LAND_MASK_LAYER_ITEM_ID =
-    '01d59c0c0fec4db0af524e041838886d';
+    'f61d078c63bf49c497a1fb0bb9ad7d71';
