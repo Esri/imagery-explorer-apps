e98826462cff94c938d8bab9a3c7b35497d5831b
Hoping Raising <hopingr7@gmail.com>
2024-11-20 16:05:20 -0500

    emit-service error

--- src/emit-explorer/components/EmitDynamicModeInfo/EmitDynamicModeInfo.tsx
+++ src/emit-explorer/components/EmitDynamicModeInfo/EmitDynamicModeInfo.tsx
@@ -16,7 +16,7 @@
 import { DynamicModeInfo } from '@shared/components/DynamicModeInfo';
 import React from 'react';
 
-export const LandsatDynamicModeInfo = () => {
+export const EmitDynamicModeInfo = () => {
     return (
         <DynamicModeInfo content="In the current map display, the most recent and most cloud free scenes from the Landsat archive are prioritized and dynamically fused into a single mosaicked image layer. As you explore, the map continues to dynamically fetch and render the best available scenes." />
     );
