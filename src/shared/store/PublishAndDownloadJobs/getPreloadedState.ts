27c2abab328665bd0c185cd73455d650b3e3c097
Jinnan Zhang <vannizhang@gmail.com>
2024-12-12 16:01:17 -0800

    feat(publish-download-jobs): enhance error handling and refactor job fetching logic

--- src/shared/store/PublishAndDownloadJobs/getPreloadedState.ts
+++ src/shared/store/PublishAndDownloadJobs/getPreloadedState.ts
@@ -10,22 +10,29 @@ import {
  */
 export const getPreloadedState4PublishAndDownloadJobs =
     async (): Promise<PublishAndDownloadJobsState> => {
-        const jobs = await getPublishAndDownloadJobsFromIndexedDB();
-        console.log('pending publish and save jobs', jobs);
+        try {
+            const jobs = await getPublishAndDownloadJobsFromIndexedDB();
+            console.log('pending publish and save jobs', jobs);
 
-        if (!jobs || jobs.length === 0) {
+            if (!jobs || jobs.length === 0) {
+                return {
+                    ...initialPublishAndDownloadJobsState,
+                };
+            }
+
+            return {
+                jobs: {
+                    byId: jobs.reduce((acc, job) => {
+                        acc[job.id] = job;
+                        return acc;
+                    }, {} as PublishAndDownloadJobsState['jobs']['byId']),
+                    allIds: jobs.map((job) => job.id),
+                },
+            };
+        } catch (error) {
+            console.error('Error fetching jobs from IndexedDB', error);
             return {
                 ...initialPublishAndDownloadJobsState,
             };
         }
-
-        return {
-            jobs: {
-                byId: jobs.reduce((acc, job) => {
-                    acc[job.id] = job;
-                    return acc;
-                }, {} as PublishAndDownloadJobsState['jobs']['byId']),
-                allIds: jobs.map((job) => job.id),
-            },
-        };
     };
