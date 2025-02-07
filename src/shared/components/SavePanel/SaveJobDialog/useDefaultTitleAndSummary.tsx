3b30359758e4abc7740d686fc1a2d57660e38433
Jinnan Zhang <vannizhang@gmail.com>
2025-02-07 14:36:11 -0800

    feat(SavePanel): update sceneId handling to support multiple scenes in titles and summaries

--- src/shared/components/SavePanel/SaveJobDialog/useDefaultTitleAndSummary.tsx
+++ src/shared/components/SavePanel/SaveJobDialog/useDefaultTitleAndSummary.tsx
@@ -10,27 +10,28 @@ export const useDefaultTitleAndSummary = (
 
     const foramttedTitle = title.split(' | ')[1] || title;
 
-    const formattedSceneId = sceneIds[0] || 'N/A';
+    const mainSceneId = sceneIds[0] || 'N/A';
+    const secondarySceneId = sceneIds[1] || 'N/A';
 
     const titleBySaveJobType: Record<PublishAndDownloadJobType, string> = {
         [PublishAndDownloadJobType.SaveWebMappingApp]: `${foramttedTitle} View`,
-        [PublishAndDownloadJobType.SaveWebMap]: `${satellite} Scene - ${formattedSceneId}`,
+        [PublishAndDownloadJobType.SaveWebMap]: `${satellite} Scene - ${mainSceneId}`,
         [PublishAndDownloadJobType.SaveWebMapWithMultipleScenes]: `${satellite} Scenes - Individual layers per scene`,
         [PublishAndDownloadJobType.SaveWebMapWithMultipleScenesInSingleLayer]: `${satellite} Scenes - A single layer with multiple scenes`,
-        [PublishAndDownloadJobType.PublishScene]: `${satellite} Scene - ${formattedSceneId}`,
-        [PublishAndDownloadJobType.PublishIndexMask]: `${satellite} Index Mask - ${formattedSceneId}`,
-        [PublishAndDownloadJobType.PublishChangeDetection]: `${satellite} Change Detection - ${formattedSceneId}`,
+        [PublishAndDownloadJobType.PublishScene]: `${satellite} Scene - ${mainSceneId}`,
+        [PublishAndDownloadJobType.PublishIndexMask]: `${satellite} Index Mask - ${mainSceneId}`,
+        [PublishAndDownloadJobType.PublishChangeDetection]: `${satellite} Change Detection - ${mainSceneId}-${secondarySceneId}`,
         // 'Download Index Mask': 'Download Index Mask',
     };
 
     const summaryBySaveJobType: Record<PublishAndDownloadJobType, string> = {
         [PublishAndDownloadJobType.SaveWebMappingApp]: `An instance of the ${foramttedTitle} app.`,
-        [PublishAndDownloadJobType.SaveWebMap]: `Web map created by ${foramttedTitle}. Scene ID: ${formattedSceneId}`,
+        [PublishAndDownloadJobType.SaveWebMap]: `Web map created by ${foramttedTitle}. Scene ID: ${mainSceneId}`,
         [PublishAndDownloadJobType.SaveWebMapWithMultipleScenes]: `Web map created by ${foramttedTitle}.`,
         [PublishAndDownloadJobType.SaveWebMapWithMultipleScenesInSingleLayer]: `Web map created by ${foramttedTitle}.`,
-        [PublishAndDownloadJobType.PublishScene]: `Hosted Imagery Servce created by ${foramttedTitle}. Scene ID: ${formattedSceneId}`,
-        [PublishAndDownloadJobType.PublishIndexMask]: `Hosted Imagery Servce created by ${foramttedTitle}. Scene ID: ${formattedSceneId}`,
-        [PublishAndDownloadJobType.PublishChangeDetection]: `Hosted Imagery Servce created by ${foramttedTitle}. Scene ID: ${formattedSceneId}`,
+        [PublishAndDownloadJobType.PublishScene]: `Hosted Imagery Servce created by ${foramttedTitle}. Scene ID: ${mainSceneId}`,
+        [PublishAndDownloadJobType.PublishIndexMask]: `Hosted Imagery Servce created by ${foramttedTitle}. Scene ID: ${mainSceneId}`,
+        [PublishAndDownloadJobType.PublishChangeDetection]: `Hosted Imagery Servce created by ${foramttedTitle}. Scene IDs: ${mainSceneId} | ${secondarySceneId}`,
         // 'Download Index Mask': '',
     };
 
