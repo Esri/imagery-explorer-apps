75684561daaa1a0f211568dfe6ad1b66e39b4268
Jinnan Zhang <vannizhang@gmail.com>
2025-02-07 16:02:49 -0800

    feat(Sentinel1SavePanel): add scene ID shortening logic for analysis mode

--- src/sentinel-1-explorer/components/Sentinel1SavePanel/useSceneIds.tsx
+++ src/sentinel-1-explorer/components/Sentinel1SavePanel/useSceneIds.tsx
@@ -1,5 +1,7 @@
 import { getSentinel1SceneByObjectId } from '@shared/services/sentinel-1/getSentinel1Scenes';
 import {
+    selectActiveAnalysisTool,
+    selectAppMode,
     selectQueryParams4MainScene,
     selectQueryParams4SceneInSelectedMode,
     selectQueryParams4SecondaryScene,
@@ -7,6 +9,39 @@ import {
 import { Sentinel1Scene } from '@typing/imagery-service';
 import React, { useEffect, useMemo, useState } from 'react';
 import { useAppSelector } from '@shared/store/configureStore';
+
+/**
+ * This function shortens the Sentinel-1 scene ID.
+ * The original scene ID is tool long to be used in the name of hosted imagery service.
+ * @param sceneId
+ * @returns
+ */
+const shortenSentinel1SceneId = (sceneId: string): string => {
+    if (!sceneId) {
+        return '';
+    }
+
+    const [
+        satellite,
+        acquisitionMode,
+        processingLevel,
+        polarization,
+        acquisitionDateStart,
+        acquisitionDateEnd,
+        orbitNumber,
+        missionId,
+        uniqueId,
+    ] = sceneId.split('_');
+
+    const parts2Keep = [
+        satellite,
+        polarization,
+        acquisitionDateStart,
+        uniqueId,
+    ].filter((part) => part !== undefined);
+
+    return parts2Keep.join('_');
+};
 
 export const useSceneIdsFromSelectedSentinel1Scenes = (): string[] => {
     const queryParams4MainScene = useAppSelector(selectQueryParams4MainScene);
@@ -16,6 +51,10 @@ export const useSceneIdsFromSelectedSentinel1Scenes = (): string[] => {
     );
 
     const [sceneIds, setSceneIds] = useState<string[]>([]);
+
+    const mode = useAppSelector(selectAppMode);
+
+    const analyzeTool = useAppSelector(selectActiveAnalysisTool);
 
     useEffect(() => {
         (async () => {
@@ -33,14 +72,23 @@ export const useSceneIdsFromSelectedSentinel1Scenes = (): string[] => {
                       )
                     : null;
 
+            let mainSceneId = mainScene?.name;
+            let secondarySceneId = secondaryScene?.name;
+
+            // Shorten the scene ID if the mode is 'analysis' and the analyze tool is 'change'.
+            if (mode === 'analysis' && analyzeTool === 'change') {
+                mainSceneId = shortenSentinel1SceneId(mainSceneId);
+                secondarySceneId = shortenSentinel1SceneId(secondarySceneId);
+            }
+
             // console.log('scene', scene);
-            setSceneIds(
-                [mainScene?.name, secondaryScene?.name].filter(Boolean)
-            );
+            setSceneIds([mainSceneId, secondarySceneId]);
         })();
     }, [
         queryParams4MainScene?.objectIdOfSelectedScene,
         queryParams4SecondaryScene?.objectIdOfSelectedScene,
+        mode,
+        analyzeTool,
     ]);
 
     return sceneIds;
