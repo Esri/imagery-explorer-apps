9fec3ebea2b70b874d1b6e90f251591cb2a580b7
Jinnan Zhang <vannizhang@gmail.com>
2025-02-14 15:02:13 -0800

    chore: add eslint and jest configurations for public test files

--- public/__tests__/public-folder.sepc.js
+++ public/__tests__/public-folder.sepc.js
@@ -4,20 +4,20 @@ const path = require("path");
 const config = require("../../src/config.json");
 
 describe("Public folder should contain thumbnail image for each app", () => {
-  // Get all app names except the ones that don't have thumbnails
-  const appNames = Object
-    .keys(config.apps)
-    .filter(appName => {
-      return appName !== "spectral-sampling-tool"
-    });
+    // Get all app names except the ones that don't have thumbnails
+    const appNames = Object
+      .keys(config.apps)
+      .filter(appName => {
+        return appName !== "spectral-sampling-tool"
+      });
 
-  const requiredFiles = appNames.map(appName => `${appName}.jpg`);
-  // console.log(requiredFiles)
+    const requiredFiles = appNames.map(appName => `${appName}.jpg`);
+    // console.log(requiredFiles)
 
-  requiredFiles.forEach(file => {
-    it(`should contain ${file}`, () => {
-      const filePath = path.join(__dirname, "../thumbnails", file);
-      expect(fs.existsSync(filePath)).toBe(true);
+    requiredFiles.forEach(file => {
+      it(`should contain ${file}`, () => {
+        const filePath = path.join(__dirname, "../thumbnails", file);
+        expect(fs.existsSync(filePath)).toBe(true);
+      });
     });
-  });
 });
