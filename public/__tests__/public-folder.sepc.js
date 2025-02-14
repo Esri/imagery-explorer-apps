dddd76c8f616ad9d3cc812dd56562c86d05555ec
Jinnan Zhang <vannizhang@gmail.com>
2025-02-14 15:04:48 -0800

    chore: rename test directory and clean up test file

--- public/__tests__/public-folder.sepc.js
+++ public/__tests__/public-folder.sepc.js
@@ -1,23 +1,20 @@
 // public-folder.spec.js
-const fs = require("fs");
-const path = require("path");
-const config = require("../../src/config.json");
+const fs = require('fs');
+const path = require('path');
+const config = require('../../src/config.json');
 
-describe("Public folder should contain thumbnail image for each app", () => {
+describe('Public folder should contain thumbnail image for each app', () => {
     // Get all app names except the ones that don't have thumbnails
-    const appNames = Object
-      .keys(config.apps)
-      .filter(appName => {
-        return appName !== "spectral-sampling-tool"
-      });
+    const appNames = Object.keys(config.apps).filter((appName) => {
+        return appName !== 'spectral-sampling-tool';
+    });
 
-    const requiredFiles = appNames.map(appName => `${appName}.jpg`);
-    // console.log(requiredFiles)
+    const requiredFiles = appNames.map((appName) => `${appName}.jpg`);
 
-    requiredFiles.forEach(file => {
-      it(`should contain ${file}`, () => {
-        const filePath = path.join(__dirname, "../thumbnails", file);
-        expect(fs.existsSync(filePath)).toBe(true);
-      });
+    requiredFiles.forEach((file) => {
+        it(`should contain ${file}`, () => {
+            const filePath = path.join(__dirname, '../thumbnails', file);
+            expect(fs.existsSync(filePath)).toBe(true);
+        });
     });
 });
