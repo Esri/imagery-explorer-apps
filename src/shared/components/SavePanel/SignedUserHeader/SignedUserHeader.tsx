f2c296cbac0f8668938a3305e5cc6ad78a9d5e43
Jinnan Zhang <vannizhang@gmail.com>
2025-05-28 18:45:06 -0700

    feat(save panel): add sign-in functionality and update UI messages for save options

--- src/shared/components/SavePanel/SignedUserHeader/SignedUserHeader.tsx
+++ src/shared/components/SavePanel/SignedUserHeader/SignedUserHeader.tsx
@@ -23,20 +23,25 @@ import './SignedUserHeader.css';
 import { useTranslation } from 'react-i18next';
 
 type Props = {
+    onSignIn: () => void;
     onSignOut: () => void;
     onSwitchAccount: () => void;
 };
 
-export const SignedUserHeader: FC<Props> = ({ onSignOut, onSwitchAccount }) => {
+export const SignedUserHeader: FC<Props> = ({
+    onSignIn,
+    onSignOut,
+    onSwitchAccount,
+}) => {
     const { t } = useTranslation();
 
     const signedInUser = getSignedInUser();
 
     const profileSettingsPageURL = getProfileSettingsURL();
 
-    if (!signedInUser) {
-        return null;
-    }
+    // if (!signedInUser) {
+    //     return null;
+    // }
 
     return (
         <div className="signed-in-user-header-container fixed top-3 left-3 flex items-center">
@@ -53,50 +58,64 @@ export const SignedUserHeader: FC<Props> = ({ onSignOut, onSwitchAccount }) => {
                 thumbnail={signedInUser.thumbnailUrl}
             /> */}
 
-            <div
-                className="w-[60px] h-[60px] border-[2px] border-custom-light-blue rounded-full z-10"
-                style={{
-                    backgroundImage: `url(${signedInUser?.thumbnailUrl})`,
-                    backgroundSize: 'cover',
-                }}
-                title={signedInUser.fullName}
-            ></div>
+            {signedInUser ? (
+                <>
+                    <div
+                        className="w-[60px] h-[60px] border-[2px] border-custom-light-blue rounded-full z-10"
+                        style={{
+                            backgroundImage: `url(${signedInUser?.thumbnailUrl})`,
+                            backgroundSize: 'cover',
+                        }}
+                        title={signedInUser.fullName}
+                    ></div>
 
-            <div className="ml-2 z-10">
-                <div className=" ">
-                    <h4 className="hover:underline">
-                        <a
-                            href={profileSettingsPageURL}
-                            target="_blank"
-                            rel="noreferrer"
-                        >
-                            {signedInUser.fullName}
-                        </a>
-                    </h4>
-                    <h5 className="text-sm">{signedInUser.portal.name}</h5>
+                    <div className="ml-2 z-10">
+                        <div className=" ">
+                            <h4 className="hover:underline">
+                                <a
+                                    href={profileSettingsPageURL}
+                                    target="_blank"
+                                    rel="noreferrer"
+                                >
+                                    {signedInUser.fullName}
+                                </a>
+                            </h4>
+                            <h5 className="text-sm">
+                                {signedInUser.portal.name}
+                            </h5>
 
-                    <div className="flex items-center text-sm">
-                        <span
-                            className="underline cursor-pointer"
-                            onClick={onSignOut}
-                        >
-                            {t('log_out')}
-                        </span>
-                        <span
-                            className="underline cursor-pointer ml-2"
-                            onClick={onSwitchAccount}
-                        >
-                            {t('switch_account')}
-                        </span>
-                    </div>
-                    {/* <calcite-icon
+                            <div className="flex items-center text-sm">
+                                <span
+                                    className="underline cursor-pointer"
+                                    onClick={onSignOut}
+                                >
+                                    {t('log_out')}
+                                </span>
+                                <span
+                                    className="underline cursor-pointer ml-2"
+                                    onClick={onSwitchAccount}
+                                >
+                                    {t('switch_account')}
+                                </span>
+                            </div>
+                            {/* <calcite-icon
                         icon="sign-out"
                         scale="s"
                         class="ml-1"
                         title="switch account"
                     ></calcite-icon> */}
-                </div>
-            </div>
+                        </div>
+                    </div>
+                </>
+            ) : (
+                <span
+                    className="underline cursor-pointer z-10"
+                    onClick={onSignIn}
+                    title={t('sign_in_to_your_account')}
+                >
+                    {t('sign_in')}
+                </span>
+            )}
         </div>
     );
 };
