c8b175f7c6c88db105d7340ca306216026f04967
Jinnan Zhang <vannizhang@gmail.com>
2025-07-11 19:03:02 -0700

    fix(landcoverexplorer): adjust z-index for download panel and ensure available years are spread into links

--- src/landcover-explorer/components/DownloadPanel/DownloadPanel.tsx
+++ src/landcover-explorer/components/DownloadPanel/DownloadPanel.tsx
@@ -53,7 +53,7 @@ const DownloadPanel = () => {
     }
 
     return (
-        <div className="download-panel absolute top-0 left-0 w-screen h-screen flex justify-center items-center bg-custom-background-95 z-20">
+        <div className="download-panel absolute top-0 left-0 w-screen h-screen flex justify-center items-center bg-custom-background-95 z-50">
             <div
                 className="h-full w-full max-w px-10 flex flex-col"
                 style={{
