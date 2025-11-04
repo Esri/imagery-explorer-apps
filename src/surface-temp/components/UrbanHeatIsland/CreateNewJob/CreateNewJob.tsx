5e26968238f0eb63c04bad43106016d9c6d7fa0d
Jinnan Zhang <vannizhang@gmail.com>
2025-11-04 08:56:15 -0800

    feat: add MonthPicker component and enhance CreateNewJob with section headers and translations

--- src/surface-temp/components/UrbanHeatIsland/CreateNewJob/CreateNewJob.tsx
+++ src/surface-temp/components/UrbanHeatIsland/CreateNewJob/CreateNewJob.tsx
@@ -2,8 +2,24 @@ import { CalciteButton, CalciteIcon } from '@esri/calcite-components-react';
 import { APP_NAME } from '@shared/config';
 import { useAppSelector } from '@shared/store/configureStore';
 import { selectSelectedUrbanAreaFeature } from '@shared/store/UrbanHeatIslandTool/selectors';
-import React from 'react';
-import { useTranslation } from 'react-i18next';
+import React, { FC } from 'react';
+import { Trans, useTranslation } from 'react-i18next';
+
+/**
+ * The header for each section in CreateNewJob component
+ * @param param0
+ * @returns
+ */
+const SectionHeader: FC<{ icon: string; text: string }> = ({ icon, text }) => (
+    <div className="flex opacity-50 mb-4">
+        <CalciteIcon icon={icon} scale="s" className="mr-1" />
+        <span className="text-sm ">{text}</span>
+    </div>
+);
+
+const SectionContent: FC<{ children: React.ReactNode }> = ({ children }) => (
+    <div className="grow">{children}</div>
+);
 
 export const CreateNewJob = () => {
     const { t } = useTranslation();
@@ -11,75 +27,107 @@ export const CreateNewJob = () => {
     const selectedFeature = useAppSelector(selectSelectedUrbanAreaFeature);
     return (
         <div className="flex justify-between w-full h-full">
-            <div className="w-1/3 h-full pr-2 mr-2">
-                <div className="flex opacity-50">
+            <div className="w-1/3 h-full px-2 mr-2 flex flex-col">
+                {/* <div className="flex opacity-50 mb-2">
                     <CalciteIcon icon="pin" scale="s" className="mr-1" />
                     <span className="text-sm ">
                         {t('choose_aoi_polygon', { ns: APP_NAME })}
                     </span>
-                </div>
+                </div> */}
+                <SectionHeader
+                    icon="pin"
+                    text={t('choose_aoi_polygon', { ns: APP_NAME })}
+                />
 
-                <div className="">
+                <SectionContent>
                     {selectedFeature ? (
-                        <div className="flex items-center">
-                            <span className="font-medium">
+                        <div className="text-sm text-opacity-80">
+                            {/* <span className="font-medium">
                                 {selectedFeature.NAME ||
                                     selectedFeature.ALL_NAMES}
-                            </span>
+                            </span> */}
+                            <Trans
+                                i18nKey="selected_aoi_polygon"
+                                ns={APP_NAME}
+                                components={{
+                                    strong: (
+                                        <strong className="font-bold text-opacity-100" />
+                                    ),
+                                }}
+                                values={{
+                                    name:
+                                        selectedFeature.NAME ||
+                                        selectedFeature.ALL_NAMES,
+                                    country: selectedFeature.COUNTRY,
+                                }}
+                            />
                         </div>
                     ) : (
-                        <span className="italic opacity-50 text-xs ">
+                        <p className="italic opacity-50 text-xs mt-2">
                             {t('no_aoi_polygon_selected', { ns: APP_NAME })}
-                        </span>
+                        </p>
                     )}
-                </div>
+                </SectionContent>
             </div>
 
-            <div className="w-1/3 h-full pr-2 mr-2">
-                <div className="flex opacity-50">
+            <div className="w-1/3 h-full px-2 mr-2 flex flex-col">
+                {/* <div className="flex opacity-50">
                     <CalciteIcon icon="calendar" scale="s" className="mr-1" />
                     <span className="text-sm ">
                         {t('choose_months', { ns: APP_NAME })}
                     </span>
-                </div>
+                </div> */}
+                <SectionHeader
+                    icon="calendar"
+                    text={t('choose_months', { ns: APP_NAME })}
+                />
+
+                <SectionContent>Month selector goes here</SectionContent>
             </div>
 
-            <div
-                className="w-1/3"
-                style={
-                    {
-                        '--calcite-button-border-color':
-                            'var(--custom-light-blue-90)',
-                        '--calcite-button-text-color':
-                            'var(--custom-light-blue)',
-                    } as React.CSSProperties
-                }
-            >
-                <div className="flex opacity-50">
+            <div className="w-1/3 h-full px-2 flex flex-col">
+                {/* <div className="flex opacity-50">
                     <CalciteIcon icon="heat-chart" scale="s" className="mr-1" />
                     <span className="text-sm ">
                         {t('start_new_siuhi_analysis', { ns: APP_NAME })}
                     </span>
-                </div>
+                </div> */}
+                <SectionHeader
+                    icon="heat-chart"
+                    text={t('start_new_siuhi_analysis', { ns: APP_NAME })}
+                />
 
-                <CalciteButton
-                    appearance="outline"
-                    scale="m"
-                    width="full"
-                    iconStart="plus"
-                    className="mb-2"
-                >
-                    {t('create_new_job', { ns: APP_NAME })}
-                </CalciteButton>
+                <SectionContent>
+                    <div
+                        style={
+                            {
+                                '--calcite-button-border-color':
+                                    'var(--custom-light-blue-90)',
+                                '--calcite-button-text-color':
+                                    'var(--custom-light-blue)',
+                            } as React.CSSProperties
+                        }
+                    >
+                        <CalciteButton
+                            appearance="outline"
+                            scale="m"
+                            width="full"
+                            iconStart="plus"
+                            className="mb-2"
+                        >
+                            {t('create_new_job', { ns: APP_NAME })}
+                        </CalciteButton>
 
-                <CalciteButton
-                    appearance="outline"
-                    scale="m"
-                    iconStart="reset"
-                    width="full"
-                >
-                    {t('reset', { ns: APP_NAME })}
-                </CalciteButton>
+                        <CalciteButton
+                            appearance="outline"
+                            scale="m"
+                            iconStart="reset"
+                            width="full"
+                        >
+                            {t('reset', { ns: APP_NAME })}
+                        </CalciteButton>
+                    </div>
+                </SectionContent>
             </div>
         </div>
     );
